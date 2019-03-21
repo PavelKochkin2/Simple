a648af48bdc2ab3f9ddcc1d061b3a1d941d6c08f
Pavel Kochkin <kach93@mail.ru>
2019-03-21 13:24:12 +0300

    created singleton

--- Program.cs
+++ Program.cs
@@ -8,13 +8,26 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            var godelPeople = new GodelPeople();
-            foreach (var person in godelPeople)
-            {
-                Console.WriteLine(person);
-            }
+            var pres1 = PresidentSingleton.GetInstance();
+            Console.WriteLine(pres1.GetHashCode());
+            var pres2 = PresidentSingleton.GetInstance();
+            Console.WriteLine(pres2.GetHashCode());
 
             Console.ReadKey();
+        }
+    }
+
+    class PresidentSingleton
+    {
+        private static PresidentSingleton _instance;
+
+        private PresidentSingleton() { }
+
+        public static PresidentSingleton GetInstance()
+        {
+            if(_instance == null)
+                _instance = new PresidentSingleton();
+            return _instance;
         }
     }
 }
